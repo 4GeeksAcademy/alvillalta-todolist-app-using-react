24a2eef47c747782224fc7b0fe53aec36a99ae7d
Arturo Villalta <alvillalt@gmail.com>
2025-05-25 16:05:14 +0000

    Termino el componente de la lista de cosas por hacer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,72 @@
-import React from "react";
+//  Hooks
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//  FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
-//create your first component
-const Home = () => {
+
+const ToDoList = () => {
+
+	//  State variables
+	const [todoInput, setTodoInput] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	//  Event handlers
+	const handleTodoInput = (event) => {
+		setTodoInput(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (todoInput.trim() != "") {
+			setTodoList([...todoList, {id: todoList.length + 1, task: todoInput}]);
+			setTodoInput("");
+		}
+	};
+
+	const handleDeleteTodo = (onClickItem) => {
+		setTodoList(todoList.filter((todoListItem) => todoListItem != onClickItem));
+	}
+
+	// Tasks left
+	function tasksLeft(todoListArray) {
+		if (todoListArray.length > 1) {
+			return todoListArray.length + " tasks left";
+		} else if (todoListArray.length === 1) {
+			return "1 task left";
+		} else return "No tasks left";
+	};
+
+
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row my-5">
+				<h1 className="text-center text-secondary">todos</h1>
+				<div className="col-10 col-md-8 col-lg-6 m-auto p-0 text-start bg-white shadow">
+					<form onSubmit={handleSubmit}>
+						<div className="p-3">
+							<input className="input-class form-control p-0 border-0 mx-1" type="text" placeholder="Type any task here"
+								onChange={handleTodoInput} value={todoInput}></input>
+						</div>
+					</form>
+					<ul className="list-group rounded-0">
+						{todoList.map((item) => {
+							return (
+								<li key={item.id} className="list-group-item p-3 border-start-0 border-end-0 d-flex justify-content-between">
+									<p className="m-0 mx-1 text-secondary">{item.task}</p>
+									<button onClick={() => handleDeleteTodo(item)} className="close-icon-class border-0 bg-transparent float-end"><FontAwesomeIcon icon={faXmark} className="text-secondary" size="xs" /></button>
+								</li>
+							);
+						})}
+						<li className="tasks-left-class list-group-item m-0 p-2 text-secondary">
+							{/* {todoList.length ? todoList.length + " tasks left" : "No tasks left"} */}
+							{tasksLeft(todoList)}</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default ToDoList;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+//  React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-//Bootstrap
+//  Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
-// index.css'
-import '../styles/index.css'
+//  CSS
+import '../styles/index.css';
 
-// components
-import Home from './components/Home';
+//  Component
+import ToDoList from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+//  Render HTML
+ReactDOM.createRoot(document.getElementById('root')).render(<ToDoList/>);
